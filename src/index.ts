b5631c599a48a84465f2afa68adb248d58c4280a
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-21 20:09:24 +0100

    21.03

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-console.log('Hello');
+// =========== 01 Intro To The DOM =========== //
+
+// console.log(window);
+// console.log(window.document);
+// console.log(document.body.innerText);
+// console.log(document.links);
+// document.write('Hello from TS');
+// console.log(document.querySelector('.container'));
+// const heading = document.querySelector('#app-title');
+// heading.innerHTML = '<h1>Hello from TS</h1>'
+
+// ==== 02 Examining The Document Object ===== //
+
+// let output;
+// output = document.all;
+// output = document.all[10];
+// output = document.all.length;
+// output = document.documentElement;
+// output = document.head;
+// output = document.body;
+// output = document.body.children;
+// output = document.doctype;
+// output = document.domain;
+// output = document.URL;
+// output = document.characterSet;
+// output = document.contentType;
+// output = document.forms;
+// output = document.forms[0];
+// output = document.forms[0].id;
+// output = document.forms[0].method;
+// output = document.forms[0].action;
+// document.forms[0].id = 'new-id';
+// output = document.links;
+// output = document.links[0];
+// output = document.links[0].href;
+// output = document.links[0].href = 'https://facebook.com';
+// output = document.links[0].id = 'google-link';
+// output = document.links[0].className = 'google-class';
+// output = document.links[0].classList;
+// output = document.images;
+// output = document.images[0];
+// output = document.images[0].src;
+
+// const forms = [...document.forms];
+
+// forms.forEach((form) => {
+//     console.log(form);
+// })
+
+// console.log(output);
+
+// ==== 03 Examining The Document Object ===== //
