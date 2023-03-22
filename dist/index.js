6127340188024c3116cafa72ee59978dfc93e49c
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-22 14:18:08 +0100

    22.03

--- dist/index.js
+++ dist/index.js
@@ -69,6 +69,97 @@
 // secondItem.innerText = 'Chuj';
 // secondItem.style.color = 'red';
 
+// const list = document.querySelector('ul');
+// const firstItem = list.querySelector('li');
+// firstItem.style.color = 'blue';
+
+// ==== 04 DOM Selectors - Multiple Elements ===== //
+
+// const listItems = document.querySelectorAll('.item');
+// console.log(listItems[1].innerText);
+// listItems[1].style.color = 'red';
+
+// listItems.forEach((item, index) => {
+// 	item.style.color = 'red';
+// 	if (index === 1) {
+// 		item.remove();
+// 	}
+
+// 	if (index === 0) {
+// 		item.innerHTML = ` Oranges
+// 					<button class="remove-item btn-link text-red">
+// 						<i class="fa-solid fa-xmark"></i>
+// 					</button>
+//         `;
+// 	}
+// });
+
+// const listItemsTwo = [...document.getElementsByClassName('item')];
+// listItemsTwo.forEach((item) => console.log(item.innerText));
+
+// const listItemsThree = document.getElementsByTagName('li');
+// console.log(listItemsThree);
+
+// ==== 05 Traversing The DOM - Elements ===== //
+
+// ===== 06 Traversing The DOM - Nodes ====== //
+
+// const div = document.createElement('div');
+// div.className = 'my-element';
+// div.id = 'my-element';
+// div.setAttribute('title', 'My Element');
+// const text = document.createTextNode('Hello World');
+// div.appendChild(text);
+
+// document.body.appendChild(div);
+// document.querySelector('ul').appendChild(div);
+
+// ======= 07 Create LI Element ======== //
+
+// Quick & Dirty
+
 const list = document.querySelector('ul');
-const firstItem = list.querySelector('li');
-firstItem.style.color = 'blue';
+
+const createItem = (item) => {
+	const li = document.createElement('li');
+	li.innerHTML = `${item} 
+    <button class="remove-item btn-link text-red">
+ 	    <i class="fa-solid fa-xmark"></i>
+    </button>`;
+	list.appendChild(li);
+};
+
+createItem('Bananas');
+
+// Apples
+// <button class="remove-item btn-link text-red">
+// 	<i class="fa-solid fa-xmark"></i>
+// </button>
+
+// Performant & Clean
+
+const createItemTwo = (item) => {
+	const li = document.createElement('li');
+	const itemText = document.createTextNode(item);
+	li.appendChild(itemText);
+	button = createButton('remove-item btn-link text-red');
+	li.appendChild(button);
+	list.appendChild(li);
+};
+
+const createButton = (classes) => {
+	const button = document.createElement('button');
+	button.className = classes;
+	icon = createIcon('fa-solid fa-xmark');
+	button.appendChild(icon);
+
+	return button;
+};
+
+const createIcon = (classes) => {
+	const icon = document.createElement('i');
+	icon.className = classes;
+	return icon;
+};
+
+createItemTwo('Pizza');
